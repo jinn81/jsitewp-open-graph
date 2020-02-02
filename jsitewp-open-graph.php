875ffc6d6446ad74543c686046b419e0c30fe142
Евгений Поздняков <pozdnyakov81@yandex.ru>
2020-02-02 21:59:00 +0300

    Update jsitewp-open-graph.php

--- jsitewp-open-graph.php
+++ jsitewp-open-graph.php
@@ -19,12 +19,12 @@ if ( ! defined ( 'ABSPATH' ) ) {
 }
 define ( 'JSITEWP_OPEN_GRAPH_URL', plugin_dir_url ( __FILE__ ) );
 function add_opengraph_doctype	( $output ) {
-    return $output . ' xmlns:og="https://opengraphprotocol.org/schema/" xmlns:fb="https://www.facebook.com/2008/fbml"';
-    }
-    add_filter ( 'language_attributes', 'add_opengraph_doctype' ) ;
+	return $output . ' xmlns:og="https://opengraphprotocol.org/schema/" xmlns:fb="https://www.facebook.com/2008/fbml"';
+	}
+	add_filter ( 'language_attributes', 'add_opengraph_doctype' ) ;
 function jinsite_og_head () {
-    global $post;
-    if ( ! is_singular () )
+	global $post;
+	if ( ! is_singular () )
 		return;
 		echo '<meta property="og:title" content="' . get_the_title () . '"/>';
 		echo '<meta property="og:type" content="article"/>';
@@ -33,11 +33,11 @@ function jinsite_og_head () {
 		if ( ! has_post_thumbnail ( $post->ID ) ) {
 		$default_logo_image = wp_get_attachment_image_src ( get_theme_mod ( 'custom_logo' ) , 'full' ); 
 		echo '<meta property="og:image" content="' . $default_logo_image . '"/>';
-    } else {
+	} else {
 		$thumbnail_src = wp_get_attachment_image_src ( get_post_thumbnail_id ( $post->ID ) , 'medium' );
 		echo '<meta property="og:image" content="' . esc_attr ( $thumbnail_src [0] ) . '"/>';
-    }
-    echo "";
-    }
+	}
+	echo "";
+	}
 add_action ( 'wp_head', 'jinsite_og_head', 5 );
 ?>
